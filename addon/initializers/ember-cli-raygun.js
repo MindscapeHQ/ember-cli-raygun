16d3d99687a042c1637581f4322363bafa91083c
Jacob Jewell <jacobjewell@eflexsystems.com>
2016-02-18 23:32:11 -0500

    allow passing in raygun options

--- addon/initializers/ember-cli-raygun.js
+++ addon/initializers/ember-cli-raygun.js
@@ -18,7 +18,7 @@ export default function(config) {
     };
 
     Raygun.init(raygunConfig.apiKey,
-                null,
+                raygunConfig.options,
                 Ember.merge(defaultCustomData, raygunConfig.customData)
                ).attach();
 
