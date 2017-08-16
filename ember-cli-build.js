f8ffdcfe48d4c2267505b1ba729cdda965019ede
Alexey Kulakov <uni_que@me.com>
2017-08-15 22:38:37 -0700

    Upgraded Ember cli to latest stable
    also fixes: ##15

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,17 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+/* eslint-env node */
+'use strict';
+
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    // Add options here
+  let app = new EmberAddon(defaults, {
+    nodeAssets: {
+      'raygun4js': {
+        srcDir: 'dist',
+        destDir: 'raygun',
+        include: ['raygun.js'],
+      }
+    }
   });
 
   /*
